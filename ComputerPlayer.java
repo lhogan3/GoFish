6217dd001460cb26fef124407cca502beb90f907
Bill T. Hogan <wthogan@uvm.edu>
2018-10-03 18:28:13 -0400

    cleaning up a bit

--- ComputerPlayer.java
+++ ComputerPlayer.java
@@ -6,9 +6,6 @@ public class ComputerPlayer extends Player {
     // computer player's memory of your cards you ask
     ArrayList<String> memory = new ArrayList<String>();
 
-    // probability the computer lies to you when you ask for a card
-    int lyingChance;
-
     // does the computer remember your cars or not
     boolean isSmart;
 
